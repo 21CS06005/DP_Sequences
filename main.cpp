c2dc88ce4ce2a2cc7a586ef9367645a54672978d
MR. AYUSH  AYUSH <91241423+21CS06005@users.noreply.github.com>
2021-09-26 14:45:38 +0000

    Commented and completed user input - ready for submission

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int* generate_random_alternating_seq(int size) {
+  int alt_seq[500];
+  cout<<"Alternating sequence generated - "<<endl;
+  for(int i = 0; i<size; i++) {
+    alt_seq[i] = (rand() % 100 + 1);
+    cout<<alt_seq[i]<<"\t";
+  }
+  cout<<endl;
+  return alt_seq;
+}
+
+// Check if sub-string is palindrome - Yes or No
 bool is_palindrome(char seq[], int start_index, int end_index) {
-  
+  // Single character is always a palindrome
   if(start_index == end_index)
     return true;
   
@@ -13,19 +25,22 @@ bool is_palindrome(char seq[], int start_index, int end_index) {
       end_index--;
     }
     else
-      return false;
+      return false; //If the characters are not same, we returns false
   }
   return true;
 }
 
+// Counting minimum number of cuts required to make each sub-string as palindrome
 int count_palindrome_cut(char seq[], int size) {
   int table[size][size];
 
+  // Initializing the table with length of the string.
   for(int m=0;m<size;m++) {
     for(int n=0;n<size;n++)
       table[m][n] = size;
   }
 
+  // Iterating for each gap - gap being the length of sub-string under consideration
   for(int gap=0; gap<size; gap++) {
     for(int i=0, j=gap; j<size; j++,i++) {
       if(i==j)
@@ -45,12 +60,15 @@ int count_palindrome_cut(char seq[], int size) {
   return table[0][size-1];
 }
 
+// Counting minimum number of insertions required to make string as palindrome
 int count_palindrome_add(char seq[], int size) {
   int table[size][size];
+  // Initializing table with 0.
   for(int l=0;l<size;l++)
     for(int k=0;k<size;k++)
       table[l][k] = 0;
 
+  // Iterating for each length - length of sub-string under consideration
   for(int len = 1; len < size; len++) {
     int i = 0;
     for(int j=len; j<size; j++) {
@@ -66,17 +84,23 @@ int count_palindrome_add(char seq[], int size) {
   return table[0][size-1];
 }
 
+// Counting number of alternate sequences
 int count_alternate_seq(int seq[], int size) {
   int last_dif = 0, alternate_count=1;
+
+  // Iterating over the input array of integers
   for(int i=1; i< size; i++) {
+    //Checking if current difference is positive and last difference was negative
     if(seq[i] > seq[i-1] && last_dif == -1) {
       alternate_count++;
       last_dif = 1;
     }
+    //Checking if current difference is positive and last difference was negative
     else if(seq[i] < seq[i-1] && last_dif == 1) {
       alternate_count++;
       last_dif = -1;
     }
+    //Checking if upto now all numbers are equal
     else if(last_dif == 0){
       if(seq[i] < seq[i-1]) {
         alternate_count++;
@@ -92,12 +116,24 @@ int count_alternate_seq(int seq[], int size) {
 }
 
 int main() {
-  char seq[] = "atyytamngerttreh";
-  int alternate_seq[] = {1,7,4,9,2,5};
+  char seq[500];
+  int size_alternate_seq;
+  srand(time(0));
 
+  cout<<"Enter the string to be made a palindrome -"<<endl;
+  cin>>seq;
+
+  cout<<"Enter the size of the alternating sequence - "<<endl;
+  cin>>size_alternate_seq;
+  
+  // Alternating sequence
+  int* alternate_seq = generate_random_alternating_seq(size_alternate_seq);
+  // Mininum number of insertions for making the string a palindrome
   int no_of_insertions = count_palindrome_add(seq, strlen(seq));
+  // Mininum number of cuts for making each sub-string a palindrome
   int no_of_cut = count_palindrome_cut(seq, strlen(seq));
-  int no_of_alternating_seq = count_alternate_seq(alternate_seq, sizeof(alternate_seq)/sizeof(int));
+  // Number of alternating sequence
+  int no_of_alternating_seq = count_alternate_seq(alternate_seq, size_alternate_seq);
 
   cout<<"Min number of insertions = "<<no_of_insertions<<endl;
   cout<<"Min number of cuts = "<<no_of_cut<<endl;
